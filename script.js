9eb399f5358b0c38216d733c0e84bfec1ab7f665
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-09-01 20:38:02 -0300

    Update

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function setColors() {
+function getRandomColor() {
+  let hexadecimal = '0123456789ABCDEF';
+  let color = '#';
 
-  const chars = "0123456789ABCDEF";
-  let color = "#";
-
-  for(let index = 1; index < 5; index += 1) {
-    color += chars.charAt(Math.floor(Math.random() * chars.length));
+  for (let cont = 0; cont < 6; cont += 1) {
+    color += hexadecimal.charAt(Math.floor(Math.random() * hexadecimal.length));
   }
   return color;
+}
+
+document.getElementById("c1").style.backgroundColor = "black";
+
+function changeColor() {
+  for(let cont = 1; cont < 4; cont += 1) {
+    let divC = "c" + (cont + 1);
+    let div = document.getElementById(divC).style.backgroundColor = getRandomColor();
+  }
 }
